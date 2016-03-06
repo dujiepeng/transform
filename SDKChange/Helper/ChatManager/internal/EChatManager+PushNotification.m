cdc86d072a2f5ce6ee2004b08ab69c8f53c3414d
sxqxyzw <sxqxwang@126.com>
2016-03-06 17:06:07 +0800

    pushoption

---
+++ SDKChange/Helper/ChatManager/internal/EChatManager+PushNotification.m
@@ -0,0 +1,13 @@
+//
+//  EChatManager+PushNotification.m
+//  SDKChange
+//
+//  Created by WYZ on 16/3/6.
+//  Copyright © 2016年 杜洁鹏. All rights reserved.
+//
+
+#import "EChatManager+PushNotification.h"
+
+@implementation EChatManager (PushNotification)
+
+@end
---
+++ SDKChange/Helper/ChatManager/internal/IChatManagerPushNotification.h
@@ -0,0 +1,13 @@
+//
+//  IChatManagerPushNotification.h
+//  SDKChange
+//
+//  Created by WYZ on 16/3/6.
+//  Copyright © 2016年 杜洁鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol IChatManagerPushNotification <NSObject>
+
+@end
